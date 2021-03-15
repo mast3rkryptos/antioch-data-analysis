ed14f1b98a484604c7237a8e462a5ba2a2526499
mast3rkryptos <mast3rkryptos@users.noreply.github.com>
2021-03-15 12:19:39 -0500

    Lots of updates
    Added some new plots, and restructured from "mass chaos" to "organized chaos". Really, it's like growing from a toddler into ... yeah the analogy got away from me; I still haven't managed to reach organized chaos.

--- antioch-data-analysis.py
+++ antioch-data-analysis.py
@@ -1,4 +1,5 @@
 import csv
+from cycler import cycler
 import dataset
 from datetime import datetime
 from dateutil import tz
@@ -7,179 +8,253 @@ import matplotlib.pyplot as plt
 import os
 import pycurl
 
-#buffer = BytesIO()
-#c = pycurl.Curl()
-#c.setopt(c.URL, 'http://ipinfo.io/43.241.71.120/privacy?token=$TOKEN')
-#c.setopt(c.WRITEDATA, buffer)
-#c.perform()
-#c.close()
-
-#body = buffer.getvalue()
-# Body is a byte string.
-# We have to know the encoding in order to print it to a text file
-# such as standard output.
-#print(body.decode('iso-8859-1'))
-#exit()
-
+# Global Variables (because I am lazy and have no face)
 dataDirectory = "C:\\Users\\gwschive\\Downloads\\ADA_Data"
-
-from_zone = tz.tzutc()
-to_zone = tz.tzlocal()
-
-print("ADA: Welcome to ADA, the Antioch Data Analysis tool\n")
-
 db = dataset.connect('sqlite:///:memory:')
-table = db['data']
+cc = (cycler(color=['tab:blue',
+                    'tab:orange',
+                    'tab:green',
+                    'tab:red',
+                    'tab:purple',
+                    'tab:brown',
+                    'tab:pink',
+                    'tab:gray',
+                    'tab:olive',
+                    'tab:cyan']) +
+      cycler(linestyle=[(0, (1, 1)),
+                        (0, (5, 5)),
+                        (0, (3, 5, 1, 5)),
+                        (0, (3, 5, 1, 5, 1, 5)),
+                        (0, (5, 1)),
+                        (0, (3, 1, 1, 1)),
+                        (0, (3, 1, 1, 1, 1, 1)),
+                        (0, (1, 10)),
+                        (0, (5, 10)),
+                        (0, (3, 10, 1, 10))]))
 
-for root, subdirs, files in os.walk(dataDirectory):
-    for filename in files:
-        if filename.endswith(".csv"):
-            with open(os.path.join(root, filename), 'r') as f:
-                firstRow = True
-                csvReader = csv.reader(f, delimiter=',', quotechar='"')
-                for row in csvReader:
-                    if firstRow:
-                        firstRow = False
-                    else:
-                        table.insert(dict(timestamp=row[0],
-                                          localDatestamp=datetime.strptime(str(row[0])[0:19], '%Y-%m-%dT%H:%M:%S').replace(tzinfo=from_zone).astimezone(to_zone).date(),
-                                          ipAddress=row[1],
-                                          city=row[2],
-                                          state=row[3],
-                                          country=row[4],
-                                          latitude=row[5],
-                                          longitude=row[6],
-                                          watchTimeMinutes=row[7],
-                                          resolution=row[8],
-                                          userAgent=row[9],
-                                          service=filename[:-4]))
+def Load_Database():
+    from_zone = tz.tzutc()
+    to_zone = tz.tzlocal()
 
-# Get count by country
-result = db.query("SELECT COUNT(ipAddress), country from data GROUP BY country ORDER BY country")
-print("Country Breakdown:")
-for row in result:
-    if row["country"] == "":
-        print(f'  <unspecified>: {row["COUNT(ipAddress)"]}')
-    else:
-        print(f'  {row["country"]}: {row["COUNT(ipAddress)"]}')
-print()
+    table = db['data']
 
-# Get count by state
-print("State Breakdown:")
-result = db.query("SELECT COUNT(ipAddress), state, country from data GROUP BY state ORDER BY state")
-for row in result:
-    if row["state"] == "":
-        print(f'  <unspecified>, {row["country"]}: {row["COUNT(ipAddress)"]}')
-    else:
-        print(f'  {row["state"]}, {row["country"]}: {row["COUNT(ipAddress)"]}')
-print()
-
-# Get count by city
-print("City Breakdown:")
-result = db.query("SELECT COUNT(ipAddress), city, state, country from data GROUP BY city ORDER BY city")
-for row in result:
-    if row["city"] == "":
-        print(f'  <unspecified>, {row["state"]}, {row["country"]}: {row["COUNT(ipAddress)"]}')
-    else:
-        print(f'  {row["city"]}, {row["state"]}, {row["country"]}: {row["COUNT(ipAddress)"]}')
-print()
-
-# Convert timestamps
-#print("Convert Timestamps:")
-#from_zone = tz.tzutc()
-#to_zone = tz.tzlocal()
-#result = db.query("SELECT timestamp from data ORDER BY timestamp")
-#for row in result:
-#    utc = datetime.strptime(str(row["timestamp"])[0:19], '%Y-%m-%dT%H:%M:%S')
-#    utc = utc.replace(tzinfo=from_zone)
-#    print("  " + str(row["timestamp"])[0:19].replace('T', ' ') + " --> " + str(utc.astimezone(to_zone)))
-#print()
-
-# Count by localDatestamp
-print("Count by Datestamp:")
-result = db.query("SELECT COUNT(localDatestamp), localDatestamp from data GROUP BY localDatestamp ORDER BY localDatestamp")
-for row in result:
-    print(f'  {row["localDatestamp"]}: {row["COUNT(localDatestamp)"]}')
-print()
+    for root, subdirs, files in os.walk(dataDirectory):
+        for filename in files:
+            if filename.endswith(".csv"):
+                with open(os.path.join(root, filename), 'r') as f:
+                    firstRow = True
+                    csvReader = csv.reader(f, delimiter=',', quotechar='"')
+                    for row in csvReader:
+                        if firstRow:
+                            firstRow = False
+                        else:
+                            table.insert(dict(timestamp=row[0],
+                                              localDatestamp=datetime.strptime(str(row[0])[0:19], '%Y-%m-%dT%H:%M:%S').replace(tzinfo=from_zone).astimezone(to_zone).date(),
+                                              ipAddress=row[1],
+                                              city=row[2],
+                                              state=row[3],
+                                              country=row[4],
+                                              latitude=row[5],
+                                              longitude=row[6],
+                                              watchTimeMinutes=row[7],
+                                              resolution=row[8],
+                                              userAgent=row[9],
+                                              service=filename[:-4],
+                                              weekNumber=
+                                              datetime.strptime(str(row[0])[0:19], '%Y-%m-%dT%H:%M:%S').replace(
+                                                  tzinfo=from_zone).astimezone(to_zone).isocalendar()[1]))
 
 
-# Count by date and service
-print("Count by Date and Service:")
-datestampList = []
-datestamps = db.query("SELECT localDatestamp FROM data GROUP BY localDatestamp ORDER BY localDatestamp")
-for datestamp in datestamps:
-    datestampList.append(datestamp["localDatestamp"])
-print(datestampList)
-services = db.query("SELECT service FROM data GROUP BY service ORDER BY service")
-for service in services:
-    data = []
+def Is_VPN():
+    #buffer = BytesIO()
+    #c = pycurl.Curl()
+    #c.setopt(c.URL, 'http://ipinfo.io/43.241.71.120/privacy?token=$TOKEN')
+    #c.setopt(c.WRITEDATA, buffer)
+    #c.perform()
+    #c.close()
+
+    #body = buffer.getvalue()
+    # Body is a byte string.
+    # We have to know the encoding in order to print it to a text file
+    # such as standard output.
+    #print(body.decode('iso-8859-1'))
+    exit()
+
+
+def CountByCountry():
+    # Get count by country
+    result = db.query("SELECT COUNT(ipAddress), country from data GROUP BY country ORDER BY country")
+    print("Country Breakdown:")
+    for row in result:
+        if row["country"] == "":
+            print(f'  <unspecified>: {row["COUNT(ipAddress)"]}')
+        else:
+            print(f'  {row["country"]}: {row["COUNT(ipAddress)"]}')
+    print()
+
+def CountByState():
+    # Get count by state
+    print("State Breakdown:")
+    result = db.query("SELECT COUNT(ipAddress), state, country from data GROUP BY state ORDER BY state")
+    for row in result:
+        if row["state"] == "":
+            print(f'  <unspecified>, {row["country"]}: {row["COUNT(ipAddress)"]}')
+        else:
+            print(f'  {row["state"]}, {row["country"]}: {row["COUNT(ipAddress)"]}')
+    print()
+
+def CountByCity():
+    # Get count by city
+    print("City Breakdown:")
+    result = db.query("SELECT COUNT(ipAddress), city, state, country from data GROUP BY city ORDER BY city")
+    for row in result:
+        if row["city"] == "":
+            print(f'  <unspecified>, {row["state"]}, {row["country"]}: {row["COUNT(ipAddress)"]}')
+        else:
+            print(f'  {row["city"]}, {row["state"]}, {row["country"]}: {row["COUNT(ipAddress)"]}')
+    print()
+
+def CountByDatestamp():
+    # Count by localDatestamp
+    print("Count by Datestamp:")
+    result = db.query("SELECT COUNT(localDatestamp), localDatestamp from data GROUP BY localDatestamp ORDER BY localDatestamp")
+    for row in result:
+        print(f'  {row["localDatestamp"]}: {row["COUNT(localDatestamp)"]}')
+    print()
+
+def CountByDateAndService():
+    # Count by date and service
+    print("Count by Date and Service:")
     datestampList = []
+    x = []
+    xcurr = 1
     datestamps = db.query("SELECT localDatestamp FROM data GROUP BY localDatestamp ORDER BY localDatestamp")
     for datestamp in datestamps:
         datestampList.append(datestamp["localDatestamp"])
-        result = db.query(f'SELECT COUNT(*) FROM data WHERE localDatestamp=\'{datestamp["localDatestamp"]}\' AND service=\'{service["service"]}\' GROUP BY localDatestamp')
-        hadRows = False
-        for row in result:
-            hadRows = True
-            data.append(row["COUNT(*)"])
-        if not hadRows:
-            datestampList.pop(-1)
-            #data.append(0)
-    print(service["service"] + " " + str(data))
-    plt.plot(datestampList, data, label=service["service"])
-plt.legend()
-plt.show()
-exit()
+        x.append(xcurr)
+        xcurr += 1
+    plt.xticks(x, datestampList, rotation='vertical')
+    print(datestampList)
+    services = db.query("SELECT service FROM data GROUP BY service ORDER BY service")
+    for service in services:
+        data = []
+        datestampList = []
+        datestamps = db.query("SELECT localDatestamp FROM data GROUP BY localDatestamp ORDER BY localDatestamp")
+        for datestamp in datestamps:
+            datestampList.append(datestamp["localDatestamp"])
+            result = db.query(f'SELECT COUNT(*) FROM data WHERE localDatestamp=\'{datestamp["localDatestamp"]}\' AND service=\'{service["service"]}\' GROUP BY localDatestamp')
+            hadRows = False
+            for row in result:
+                hadRows = True
+                data.append(row["COUNT(*)"])
+            if not hadRows:
+                datestampList.pop(-1)
+        print(service["service"] + " " + str(data))
+        plt.plot(datestampList, data, label=service["service"])
+        for x, y in zip(datestampList, data):
+            label = "{:d}".format(y)
+            plt.annotate(label, (x, y), textcoords="offset points", xytext=(0, 10), ha='center')
+    plt.legend()
+    plt.title("Viewer Count by Service")
+    plt.xlabel("Date")
+    plt.ylabel("Viewer Count")
+    # COLORBLIND EXPERIMENT plt.rc('axes', prop_cycle=cc)
+    plt.show()
+    print()
 
-
-for datestamp in datestamps:
-    print(datestamp["localDatestamp"])
-    result = db.query(f'SELECT COUNT(*), service FROM data WHERE localDatestamp=\'{datestamp["localDatestamp"]}\' GROUP BY service ORDER BY service')
+def CountByWeekNumberAndLocation():
+    # Create a chart showing X: ViewerCount, Y: weekNumber, and Legend: Location
+    print("Count by Location")
+    locations = []
+    x = []
+    y = []
+    # City-wise
+    result = db.query("SELECT state, city FROM data GROUP BY state, city ORDER BY state, city")
+    # State-wise
+    #result = db.query("SELECT state FROM data GROUP BY state ORDER BY state")
     for row in result:
-        print(row)
-exit()
+        # City-wise
+        locations.append(f'{row["city"]}, {row["state"]}')
+        # State-wise
+        #locations.append(f'{row["state"]}')
+    result = db.query("SELECT weekNumber FROM data GROUP BY weekNumber ORDER BY weekNumber")
+    for row in result:
+        x.append(row["weekNumber"])
+    for location in locations:
+        # Remove Cedar Rapids and Marion from graph
+        if location == "Cedar Rapids, IA" or location == "Marion, IA":
+            continue
+        y = []
+        for currentWeekNumber in x:
+            # City-wise
+            result = db.query(f'SELECT COUNT(*), weekNumber FROM data WHERE state=\'{location.split(", ")[1]}\' AND city=\'{location.split(", ")[0]}\' GROUP BY weekNumber ORDER BY weekNumber')
+            # State-wise
+            #result = db.query(f'SELECT COUNT(*), weekNumber FROM data WHERE state=\'{location}\' GROUP BY weekNumber ORDER BY weekNumber')
+            dataFound = False
+            for row in result:
+                if row["weekNumber"] == currentWeekNumber:
+                    y.append(row["COUNT(*)"])
+                    dataFound = True
+            if not dataFound:
+                y.append(0)
+        plt.plot(x, y, label=location)
+    plt.legend()
+    plt.show()
+    print()
+
+# This function excludes Cedar Rapids, Marion, and Hiawatha
+def CountTopTenLocations():
+    weekNumbers = GetWeekNumbers()
+
+    count = 0
+    locations = db.query('SELECT COUNT(*), city, state FROM data WHERE city<>\'\' AND city<>\'Cedar Rapids\'  AND city<>\'Marion\'  AND city<>\'Hiawatha\' GROUP BY city, state ORDER BY COUNT(*) DESC')
+    for row in locations:
+        if count >= 10:
+            break
+        count += 1
+        print(f'{row["city"]}, {row["state"]} : {row["COUNT(*)"]}')
+        index = 0
+        x = []
+        y = []
+        locationStats = db.query(f'SELECT COUNT(*), weekNumber FROM data WHERE city=\'{row["city"]}\' AND state=\'{row["state"]}\' GROUP BY weekNumber ORDER BY weekNumber')
+        for rowStats in locationStats:
+            while weekNumbers[index] < rowStats["weekNumber"]:
+                x.append(str(weekNumbers[index]))
+                y.append(0)
+                index += 1
+            print(f'  {rowStats["weekNumber"]}: {rowStats["COUNT(*)"]}')
+            x.append(str(rowStats["weekNumber"]))
+            y.append(rowStats["COUNT(*)"])
+            index += 1
+        print(x)
+        print(y)
+        plt.plot(x, y, label=f'{row["city"]}, {row["state"]}')
+    plt.legend()
+    plt.title("Top Ten Remote Locations (excludes Cedar Rapids, Marion, & Hiawatha)")
+    plt.xlabel("ISO Week Number")
+    plt.ylabel("Viewer Count")
+    plt.show()
+    print()
 
 
 
+def GetWeekNumbers():
+    ret = []
+    result = db.query("SELECT weekNumber FROM data GROUP BY weekNumber ORDER BY weekNumber")
+    for row in result:
+        ret.append(row["weekNumber"])
+    return ret
 
-result = db.query("SELECT COUNT(*), localDatestamp, service FROM data WHERE watchTimeMinutes > 7 GROUP BY localDatestamp, service ORDER BY localDatestamp, service")
-for row in result:
-    if row["service"] not in data.keys():
-        data[row["service"]] = []
-    data[row["service"]].append(row["COUNT(*)"])
-    if row["localDatestamp"] not in datestamps:
-        datestamps.append(row["localDatestamp"])
-    print(f'  {row["localDatestamp"]} - {row["service"]}: {row["COUNT(*)"]}')
-print()
-# Plot
-print(data)
-print(datestamps)
-exit()
-plt.plot()
-plt.legend()
-plt.show()
+def GetDatestamps():
+    ret = []
+    result = db.query("SELECT localDatestamp FROM data GROUP BY localDatestamp ORDER BY localDatestamp")
+    for row in result:
+        ret.append(row["localDatestamp"])
+    return ret
 
-
-# DON'T NEED A PLOT FOR NOW
-exit()
-# Display a state-wise plot (in-progress)
-plt.style.use('ggplot')
-result = db.query("SELECT COUNT(ipAddress), state, country from data GROUP BY state ORDER BY state")
-states = []
-viewers = []
-for row in result:
-    if row["state"] == "":
-        states.append("<unspecified>, " + row["country"])
-    else:
-        states.append(row["state"] + ", " + row["country"])
-    viewers.append(row["COUNT(ipAddress)"])
-x_pos = [i for i, _ in enumerate(states)]
-plt.bar(x_pos, viewers)
-plt.xlabel("State")
-plt.ylabel("Viewer Count")
-plt.title("Viewer Count by State")
-plt.xticks(x_pos, states, rotation=60)
-plt.savefig(os.path.join(dataDirectory, "plot.png"))
-plt.show()
-exit()
-
+if __name__ == "__main__":
+    print("ADA: Welcome to ADA, the Antioch Data Analysis tool\n")
+    Load_Database()
+    CountByDateAndService()
+    #CountByWeekNumberAndLocation()
+    CountTopTenLocations()
